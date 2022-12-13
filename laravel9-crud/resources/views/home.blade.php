f2196f57c6f4515f16feb4cd3e1755b69f6b710d
NickvanLoo28 <58033512+NickvanLoo@users.noreply.github.com>
2022-12-13 13:55:45 +0100

    Update

---
+++ laravel9-crud/resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.layout')
+@section('content')
+    <div class="container">
+        <h1>Welkom op mijn Laravel 9 CRUD Applicatie</h1>
+        <p> <a href="/student">Klik hier</a> om naar de applicatie te gaan.</p>
+    </div>
+@endsection
